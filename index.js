d365c5b9e7ad55e56306c4163c939d87c54ae5fd
Alan Grosz <lalogrosz@gmail.com>
2021-06-07 08:34:42 -0300

    pruebas

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ var slackEventRouter = require('./routes/slack-events');
 
 var app = express();
 
+var debug = require('debug')('ranking-names:server');
+var http = require('http');
+const mongoose = require("mongoose")
+var onstart = require('./onstart');
+
+require('dotenv').config();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
@@ -39,4 +46,92 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+/**
+ * Get port from environment and store in Express.
+ */
+
+ var port = normalizePort(process.env.PORT || '80');
+ app.set('port', port);
+ 
+ /**
+  * Create HTTP server.
+  */
+ 
+ var server = http.createServer(app);
+ 
+ /**
+  * Listen on provided port, on all network interfaces.
+  */
+ const mongoPass = process.env.MONGO_DB_PASS;
+  mongoose
+   .connect(
+     "mongodb+srv://rankingnames:" +  mongoPass + "@cluster0.sopzt.mongodb.net/ranking_names?retryWrites=true&w=majority"
+     , { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => {
+       server.listen(port);
+   });
+ 
+ server.on('error', onError);
+ server.on('listening', onListening);
+ 
+ /**
+  * Normalize a port into a number, string, or false.
+  */
+ 
+ function normalizePort(val) {
+   var port = parseInt(val, 10);
+ 
+   if (isNaN(port)) {
+     // named pipe
+     return val;
+   }
+ 
+   if (port >= 0) {
+     // port number
+     return port;
+   }
+ 
+   return false;
+ }
+ 
+ /**
+  * Event listener for HTTP server "error" event.
+  */
+ 
+ function onError(error) {
+   if (error.syscall !== 'listen') {
+     throw error;
+   }
+ 
+   var bind = typeof port === 'string'
+     ? 'Pipe ' + port
+     : 'Port ' + port;
+ 
+   // handle specific listen errors with friendly messages
+   switch (error.code) {
+     case 'EACCES':
+       console.error(bind + ' requires elevated privileges');
+       process.exit(1);
+       break;
+     case 'EADDRINUSE':
+       console.error(bind + ' is already in use');
+       process.exit(1);
+       break;
+     default:
+       throw error;
+   }
+ }
+ 
+ /**
+  * Event listener for HTTP server "listening" event.
+  */
+ 
+ function onListening() {
+   var addr = server.address();
+   var bind = typeof addr === 'string'
+     ? 'pipe ' + addr
+     : 'port ' + addr.port;
+   debug('Listening on ' + bind);
+   onstart.loadMembers();
+ }
+
